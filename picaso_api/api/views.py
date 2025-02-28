3a7c7ba8834de4359d014a51a0856e9fce33fdfe
Gautam Kabiraj <gautamkabiraj@gmail.com>
2025-03-01 01:19:47 +0530

    django rest init

---
+++ picaso_api/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def hello_world(request):
+    return Response({"message": "Hello, Django REST Framework!"})
+
+
+# Create your views here.
