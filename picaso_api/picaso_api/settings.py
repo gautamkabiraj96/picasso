13edc7465512da76ce340f82fb346ff6e15667bb
Gautam Kabiraj <gautamkabiraj@gmail.com>
2025-03-02 14:32:33 +0530

    add users model

--- picaso_api/picaso_api/settings.py
+++ picaso_api/picaso_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tasks',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
