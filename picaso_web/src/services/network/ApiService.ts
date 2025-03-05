df0696ff86713b2692f7dd8a6ceef9f3882f76a9
Gautam Kabiraj <gautamkabiraj@gmail.com>
2025-03-06 01:41:47 +0530

    format

--- picaso_web/src/services/network/ApiService.ts
+++ picaso_web/src/services/network/ApiService.ts
@@ -1,29 +1,37 @@
-import AxiosInstance from './AxiosInstance';
-import { INetworkService, ApiRequestConfig } from './INetworkService';
+import AxiosInstance from './AxiosInstance'
+import { INetworkService, ApiRequestConfig } from './INetworkService'
 
 export class ApiService implements INetworkService {
- async get<T>(url: string, config?: ApiRequestConfig): Promise<T> {
-  const response = await AxiosInstance.get<T>(url, config);
-  return response.data;
- }
+  async get<T>(url: string, config?: ApiRequestConfig): Promise<T> {
+    const response = await AxiosInstance.get<T>(url, config)
+    return response.data
+  }
 
- async post<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T> {
-  const response = await AxiosInstance.post<T>(url, data, config);
-  return response.data;
- }
+  async post<T>(
+    url: string,
+    data?: any,
+    config?: ApiRequestConfig
+  ): Promise<T> {
+    const response = await AxiosInstance.post<T>(url, data, config)
+    return response.data
+  }
 
- async put<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T> {
-  const response = await AxiosInstance.put<T>(url, data, config);
-  return response.data;
- }
+  async put<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T> {
+    const response = await AxiosInstance.put<T>(url, data, config)
+    return response.data
+  }
 
- async patch<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T> {
-  const response = await AxiosInstance.patch<T>(url, data, config);
-  return response.data;
- }
+  async patch<T>(
+    url: string,
+    data?: any,
+    config?: ApiRequestConfig
+  ): Promise<T> {
+    const response = await AxiosInstance.patch<T>(url, data, config)
+    return response.data
+  }
 
- async delete<T>(url: string, config?: ApiRequestConfig): Promise<T> {
-  const response = await AxiosInstance.delete<T>(url, config);
-  return response.data;
- }
+  async delete<T>(url: string, config?: ApiRequestConfig): Promise<T> {
+    const response = await AxiosInstance.delete<T>(url, config)
+    return response.data
+  }
 }
--- picaso_web/src/services/network/INetworkService.ts
+++ picaso_web/src/services/network/INetworkService.ts
@@ -1,16 +1,16 @@
-import { InternalAxiosRequestConfig, CancelTokenSource } from 'axios';
+import { InternalAxiosRequestConfig, CancelTokenSource } from 'axios'
 
 export interface ApiRequestConfig extends InternalAxiosRequestConfig<any> {
-	retry?: number;
-	cancellable?: boolean;
-	cancelTokenSource?: CancelTokenSource;
-	token?: string;
+  retry?: number
+  cancellable?: boolean
+  cancelTokenSource?: CancelTokenSource
+  token?: string
 }
 
 export interface INetworkService {
-	get<T>(url: string, config?: ApiRequestConfig): Promise<T>;
-	post<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T>;
-	put<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T>;
-	patch<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T>;
-	delete<T>(url: string, config?: ApiRequestConfig): Promise<T>;
+  get<T>(url: string, config?: ApiRequestConfig): Promise<T>
+  post<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T>
+  put<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T>
+  patch<T>(url: string, data?: any, config?: ApiRequestConfig): Promise<T>
+  delete<T>(url: string, config?: ApiRequestConfig): Promise<T>
 }
