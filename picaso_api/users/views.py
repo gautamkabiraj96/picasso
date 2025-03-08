6c0777cb4ea0a3be227acff4d99e7a0bd8c657af
Gautam Kabiraj <gautamkabiraj@gmail.com>
2025-03-08 22:02:13 +0530

    anon user on login

--- picaso_api/users/views.py
+++ picaso_api/users/views.py
@@ -1,28 +1,34 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
 User = get_user_model()
 
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 class LoginView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
-        user = User.objects.filter(email=email).first()
+        
+        user = authenticate(request, email=email, password=password)
 
-        if user and user.check_password(password):
+        if user is not None:
             refresh = RefreshToken.for_user(user)
             return Response({
                 "accessToken": str(refresh.access_token),
                 "refreshToken": str(refresh),
             })
+        
         return Response({"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
 class TokenRefreshView(APIView):
