361e667dff0a129429f4334e0bf75f1ba809346f
tsemi ongyal <tsemi@tsemis-MacBook-Air.local>
2025-03-12 23:52:52 +0530

    enabled corsheaders for local client-side application

--- picaso_api/picaso_api/settings.py
+++ picaso_api/picaso_api/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
     'tasks',
     'users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -167,3 +169,6 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+# CORS settings
+CORS_ALLOWED_ORIGINS=['http://localhost:3000']
